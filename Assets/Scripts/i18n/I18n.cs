1e1692e949608018a1ca1c132d80104e775b182c
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2017-11-19 17:04:17 +0100

    Prepare folder architecture

---
+++ Assets/Scripts/i18n/I18n.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarsFrenzy
+{
+    public class I18n : Mgl.I18n
+    {
+        protected static readonly I18n instance = new I18n();
+
+        // Customize your languages here
+        protected static string[] locales = new string[] {
+          "en-US",
+          "fr-FR"
+        };
+
+        public static I18n Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
