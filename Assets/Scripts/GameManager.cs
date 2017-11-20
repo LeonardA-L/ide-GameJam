a83daef55dbe28679b1c4de61986cb2bd48cb24b
Léonard Allain-Launay <LeonardA-L@users.noreply.github.com>
2017-11-20 08:29:43 +0100

    Add GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarsFrenzy
+{
+    public class GameManager : MonoBehaviour
+    {
+        protected static readonly GameManager instance = new GameManager();
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public static GameManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+}
