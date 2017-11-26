e60874694008e6b8f6af4ca977bc69cd0bb94831
LeonardA-L <leonard@allain-launay.fr>
2017-11-26 22:49:50 +0100

    Add simple camera controller to follow the player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarsFrenzy
+{
+    public class CameraController : MonoBehaviour
+    {
+        public Transform character;
+        private Vector3 positionOffset;
+        private Camera cam;
+
+        public bool lockX = true;
+        public bool lockZ = true;
+        public bool active = false;
+
+        public Vector3 disp;
+
+        // Use this for initialization
+        void Start()
+        {
+            cam = GetComponent<Camera>();
+            positionOffset = transform.position - character.position;
+            active = true;
+
+            lockX = true;
+            lockZ = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            disp = (character.position + positionOffset) - transform.position;
+
+            disp = transform.InverseTransformVector(disp);
+            disp.x = lockX ? 0 : disp.x;
+            disp.z = lockZ ? 0 : disp.z;
+            disp.y = lockZ ? 0 : disp.y;
+
+            transform.localPosition += disp;
+        }
+    }
+}
