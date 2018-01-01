cc21b94d10ce990dd4c09e0397a2b91457596dee
LeonardA-L <leonard@allain-launay.fr>
2018-01-01 11:23:48 +0100

    Refactor AudioManager to fetch all sounds on startup

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,23 +7,6 @@ namespace MarsFrenzy
     public class AudioManager : MonoBehaviour
     {
         protected static AudioManager instance;
-        // General sounds
-        public AudioSource characterWalking;
-        //public AudioSource events;
-        public AudioSource communication;
-        //public AudioSource alarm;
-        public AudioSource win;
-        public AudioSource gameOver;
-
-        // Module specific
-        //public AudioSource moduleFull;
-        public AudioSource moduleRepairs;
-        public AudioSource moduleStopProduction;
-        public AudioSource moduleUpdate;
-        public AudioSource modulePotato;
-        public AudioSource moduleWater;
-        public AudioSource moduleElectricity;
-
         private Dictionary<string, AudioSource> sounds;
 
         // Use this for initialization
@@ -32,21 +15,16 @@ namespace MarsFrenzy
             instance = this;
             sounds = new Dictionary<string, AudioSource>();
 
-            sounds.Add("characterWalking", characterWalking);
-            //sounds.Add("events", events);
-            sounds.Add("communication", communication);
-            //sounds.Add("alarm", alarm);
-            sounds.Add("win", win);
-            sounds.Add("gameOver", gameOver);
+            GameObject[] soundObjects = GameObject.FindGameObjectsWithTag("Sound");
 
-            // Module specific
-            //sounds.Add("moduleFull", moduleFull);
-            sounds.Add("moduleRepairs", moduleRepairs);
-            sounds.Add("moduleStopProduction", moduleStopProduction);
-            sounds.Add("moduleUpdate", moduleUpdate);
-            sounds.Add("modulepotatoes", modulePotato);
-            sounds.Add("modulewater", moduleWater);
-            sounds.Add("moduleelectricity", moduleElectricity);
+            foreach (GameObject sound in soundObjects)
+            {
+                AudioSource source = sound.GetComponent<AudioSource>();
+                if(source != null)
+                {
+                    sounds.Add(sound.name, source);
+                }
+            }
         }
 
         public static AudioManager Instance
