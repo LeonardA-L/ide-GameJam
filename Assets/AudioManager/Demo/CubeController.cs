6ea269b3e76d76729aa1ec1729dde964f8f39a25
LeonardA-L <leonard@allain-launay.fr>
2018-09-09 13:09:31 +0200

    Audiomanager - Improve

---
+++ Assets/AudioManager/Demo/CubeController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // Will be called when the user clicks on the cube
+    void OnMouseDown ()
+    {
+        Debug.Log("Clicked on cube, playing sound");
+
+        // Call AudioManager
+        AudioManager.Instance.PlaySound("soundName");
+
+        AudioManager.Instance.StopAllSounds();
+    }
+
+}
